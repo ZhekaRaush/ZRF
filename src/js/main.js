abd80fe4a453adafc391a0a0223558c5eb313769
Zheka@RAUSHWEB <rausch08@yandex.ru>
2023-01-14 19:32:31 +0300

    Добавлен новый файл:   src/js/lib/modules/actions.js Добавлен новый файл:   src/js/lib/modules/handler.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,39 @@
-import './lib/lib';
+import $ from './lib/lib';
 
 // console.log($('div'));
 // $('.active').hide();
 // $('.active').hide().show();
-$('.active').toggle();
+// $('.active').toggle();
+// Добавление классов
+// $('.active').addClass('hello', 'world');
+// Удаление классов
+// $('.active').removeClass('hello', 'world');
+// toggle 
+// $('.active').toggleClass('hello');
+// $('.active').toggleClass('world');
+
+// $('.active').on('click', hello);
+// $('.active').off('click', hello);
+
+// $('.active').click(hello);
+
+// function hello(){
+//     console.log('hello');
+// }
+
+// $('button').on('click', function() {
+//     $(this).toggleClass('active');
+// });
+
+// // console.log($('button').html());
+// console.log($('button').layout('hello'));
+
+$('button').on('click', function() {
+    $('div').listElement(2).toggleClass('active');
+});
+
+$('div').click(function() {
+    console.log($(this).index());
+});
+
+console.log($('div').listElement(2).find('.some'));
